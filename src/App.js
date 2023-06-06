915d3dc57d4c3ec6c454ff078cf9f5a3473197c2
Alex JIANG <alex.jiang@grenoble-inp.org>
2023-06-06 18:42:09 +0200

    [DEV] Example using Route and Link.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import './App.css';
-import {useState} from "react";
-import Axios from "axios"
 
 function App() {
-
-  const [excuse, setExcuse] = useState("");
-
-  const fetchExcuse = (excuseType) => {
-    Axios.get(`https://excuser.herokuapp.com/v1/excuse/${excuseType}/`).then((res) => {
-      setExcuse(res.data[0].excuse);
-    });
-  };
-
-  return(
-    <div className="App">
-      <h1>Generate An Excuse</h1>
-
-      <button onClick={() => fetchExcuse("party")}>
-        Party
-      </button>
-
-      <button onClick={() => fetchExcuse("family")}>
-        Family
-      </button>
-
-      <button onClick={() => fetchExcuse("office")}>
-        Office
-      </button>
-      <h1>Excuse :{excuse}</h1>
-    </div>
-  );
+  return (
+  <div className="App">
+    Hello world
+  </div>
+  ); 
 };
 
 
-export default App;
+export default App; 
