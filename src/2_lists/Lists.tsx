4fa512286091ebc98ac6d911c3a01e17198fca4a
Alex JIANG <alex.jiang@grenoble-inp.org>
2023-06-13 18:16:21 +0200

    [Migrate] Lists and useState example to tsx.

--- src/2_lists/Lists.tsx
+++ src/2_lists/Lists.tsx
@@ -2,7 +2,11 @@ const names : string[] = ["James", "Jake", "Pedro", "Alex"]
 
 const forEachElement = () => {
     // Return in a h1 tag each name of the array.
-    names.map((name) => {
-        return <h1>{name}</h1>
-    });
+    return (
+        <div>
+            {names.map((name) => {
+                return <h1>{name}</h1>
+            })}
+        </div>
+    );
 }
