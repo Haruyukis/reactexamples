c506ef338920eec913e70567c0f5e88f983de1f1
Alex JIANG <alex.jiang@grenoble-inp.org>
2023-06-22 18:05:52 +0200

    [Dev] Use and call a custom counter Hook in TP.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css';
-import { Form } from "./Form";
+import { useCount } from "./Counter";
 
 function App(): JSX.Element {
 
+  const [counter, increase, decrease, reset] = useCount(0); // ⚠️ Basic call of a Hook
+  // Remark: These name must be the same if the hook return an object. Can be change if it's an array.
+
   return (
     <div className="App">
-      <Form /> 
+      <h1>{counter}</h1>
+      <button onClick={() => increase()}>Increase</button>
+      <button onClick={() => decrease()}>Decrease</button>
+      <button onClick={() => reset()}>Reset</button>
     </div>
   );
 }
