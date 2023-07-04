ddd0c6c9b363c21987ae8f87fcc3956558654ff2
Alex Jiang <alex.jiang@grenoble-inp.org>
2023-07-04 16:43:01 +0200

    [Dev] Non Persistent DarkMode.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { SideBar } from "./components/SideBar";
 import "./App.css";
+import { DarkMode } from "./components/DarkMode";
+import { SideBar } from "./components/SideBar";
+import { useState } from "react";
 
 function App(): JSX.Element {
+  const [isDark, setIsDark] = useState("dark");
+
   return (
-    <div className={`v-screen flex h-screen bg-gray-500`}>
-      <SideBar />
+    <div className={isDark}>
+      <div className={`v-screen flex h-screen bg-gray-200 dark:bg-gray-700`}>
+        <SideBar />
+        <DarkMode set={setIsDark} isDark={isDark} />
+      </div>
     </div>
   );
 }
--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -1,6 +1,23 @@
-export const DarkMode = () => {
-  // Render
-  return {
-    /* Title */
+//<button onClick={}></button>
+
+import { useEffect } from "react";
+
+interface IDarkModeProps {
+  set: React.Dispatch<React.SetStateAction<string>>;
+  isDark: string;
+}
+
+export const DarkMode = (props: IDarkModeProps) => {
+  const handleDarkMode = () => {
+    props.isDark === "" ? props.set("dark") : props.set("");
+    console.log(props.isDark);
   };
+
+  return (
+    <div className="mx-auto my-auto flex h-5 w-5">
+      <button onClick={handleDarkMode}>
+        {props.isDark === "dark" ? <>☀️</> : <>🌚</>}
+      </button>
+    </div>
+  );
 };
