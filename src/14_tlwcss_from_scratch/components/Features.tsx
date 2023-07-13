f87c84d7304820a1f5265f28599f8ac42c361342
Alex Jiang <alex.jiang@grenoble-inp.org>
2023-07-13 19:01:04 +0200

    [Fix] Fix mistypes

--- src/14_tlwcss_from_scratch/components/Features.tsx
+++ src/14_tlwcss_from_scratch/components/Features.tsx
@@ -82,7 +82,7 @@ export const Features = () => {
               Everything you need in one place
             </h3>
             <p className="text-darkGrayishBlue">
-              Stop jumping from one service to another to communicate,, store
+              Stop jumping from one service to another to communicate, store
               files, track tasks and share documents. Manage offers an
               all-in-one team productivity solution.
             </p>
